56f1f8fb854aadf404e1e54faccd1c825b3de893
melese-2121 <meleseayen2016@gmail.com>
2024-09-30 05:10:03 +0300

    Finish entities and usecases

---
+++ on-boarding/NumberTriviaApp/entities_and_usecases/test/features/domain/usecases/get_concrete_number_trivia_test.dart
@@ -0,0 +1,16 @@
+import 'package:project_structure/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockNumberTriviaRepository extends Mock
+    implements NumberTriviaRepository {}
+
+void main() {
+  GetConcreteNumberTrivia usecase;
+  MockNumberTriviaRepository mockNumberTriviaRepository;
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  });
+}
