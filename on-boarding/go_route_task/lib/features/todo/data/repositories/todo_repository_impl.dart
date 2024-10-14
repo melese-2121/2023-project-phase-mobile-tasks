338ed26b629a95239a325375e79d33545263b3ba
melese-2121 <meleseayen2016@gmail.com>
2024-10-14 05:07:54 +0300

    finsih connection checker

--- on-boarding/go_route_task/lib/features/todo/data/repositories/todo_repository_impl.dart
+++ on-boarding/go_route_task/lib/features/todo/data/repositories/todo_repository_impl.dart
@@ -1,56 +1,54 @@
-import 'package:connectivity/connectivity.dart';
+// data/repositories/todo_repository_impl.dart
+import 'package:your_project/core/network/network_info.dart';
 import '../../domain/entities/todo.dart';
 import '../../domain/repositories/todo_repository.dart';
 import '../datasources/todo_remote_data_source.dart';
 
-
-
 class TodoRepositoryImpl implements TodoRepository {
   final TodoRemoteDataSource remoteDataSource;
   final TodoLocalDataSource localDataSource;
-  final Connectivity connectivity;
+  final NetworkInfo networkInfo;
 
   TodoRepositoryImpl({
     required this.remoteDataSource,
     required this.localDataSource,
-    required this.connectivity,
+    required this.networkInfo,
   });
 
   @override
   Future<List<Todo>> getAllTodos() async {
-    if (await connectivity.checkConnectivity() != ConnectivityResult.none) {
-      // Fetch from remote if online
+    if (await networkInfo.isConnected) {
       final todos = await remoteDataSource.fetchTodosFromServer();
-      // Optionally, cache the remote data locally
-      todos.forEach(localDataSource.addTodoToDb);
+      todos.forEach(localDataSource.addTodoToDb); // Cache the remote data
       return todos;
     } else {
-      // Fetch from local storage if offline
-      return await localDataSource.fetchTodosFromDb();
+      return await localDataSource
+          .fetchTodosFromDb(); // Fetch from local if offline
     }
   }
 
   @override
   Future<void> addTodo(Todo todo) async {
-    if (await connectivity.checkConnectivity() != ConnectivityResult.none) {
+    if (await networkInfo.isConnected) {
       await remoteDataSource.addTodoToServer(todo);
     }
-    await localDataSource.addTodoToDb(todo);
+    await localDataSource.addTodoToDb(todo); // Always add to local storage
   }
 
   @override
   Future<void> deleteTodoById(String id) async {
-    if (await connectivity.checkConnectivity() != ConnectivityResult.none) {
+    if (await networkInfo.isConnected) {
       await remoteDataSource.deleteTodoFromServer(id);
     }
-    await localDataSource.deleteTodoFromDb(id);
+    await localDataSource
+        .deleteTodoFromDb(id); // Always delete from local storage
   }
 
   @override
   Future<void> updateTodo(Todo todo) async {
-    if (await connectivity.checkConnectivity() != ConnectivityResult.none) {
+    if (await networkInfo.isConnected) {
       await remoteDataSource.updateTodoOnServer(todo);
     }
-    await localDataSource.updateTodoInDb(todo);
+    await localDataSource.updateTodoInDb(todo); // Always update local storage
   }
 }
--- on-boarding/number_trivia_tdd_clean_architecture/android/app/src/main/kotlin/com/example/number_trivia_tdd_clean_architecture/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.number_trivia_tdd_clean_architecture
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
